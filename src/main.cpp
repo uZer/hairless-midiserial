34a3b5fd54c768c77b39bd3bdcba07be961f9e27
rhargreaves <robert@roberthargreaves.net>
2020-05-03 14:34:27 +0100

    Add command line parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <QtWidgets/QApplication>
+#include <QCommandLineParser>
 #include "ui/mainwindow.h"
+
+#include <QTextStream>
 
 int main(int argc, char *argv[])
 {
@@ -7,6 +10,22 @@ int main(int argc, char *argv[])
     QCoreApplication::setOrganizationName("Projectgus");
     QCoreApplication::setOrganizationDomain("projectgus.com");
     QCoreApplication::setApplicationName("Hairless MIDI Serial Bridge");
+    QCoreApplication::setApplicationVersion(QString::fromUtf8(VERSION));
+
+    QCommandLineParser parser;
+    parser.setApplicationDescription("Sends and receives MIDI events over a serial device.");
+    parser.addHelpOption();
+    parser.addVersionOption();
+    QCommandLineOption showDebugOption(QStringList() << "d" << "debug",
+                                       QCoreApplication::translate("main", "Show debug MIDI messages"));
+    parser.addOption(showDebugOption);
+    parser.process(a);
+
+//    QTextStream out(stdout);
+//    out << QString("Hello, World!") << endl;
+
+    bool showDebug = parser.isSet(showDebugOption);
+
     MainWindow w;
     w.show();
     return a.exec();
